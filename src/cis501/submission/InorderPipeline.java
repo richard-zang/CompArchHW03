4eeac08b5023bc31fa6a020070da9c9794da6f61
gatoWololo <gatoWololo@gmail.com>
2016-09-20 23:35:34 -0400

    Added ignore file. Cleaned up code.

--- src/cis501/submission/InorderPipeline.java
+++ src/cis501/submission/InorderPipeline.java
@@ -38,126 +38,143 @@ public class InorderPipeline implements IInorderPipeline {
      *                             stalling to resolve all data hazards.
      */
     public InorderPipeline(int additionalMemLatency, Set<Bypass> bypasses) {
-		this.additionalMemLatency = additionalMemLatency;
-		this.bypasses = bypasses;
-		latches = new Insn[5];
+        this.additionalMemLatency = additionalMemLatency;
+        this.bypasses = bypasses;
+        latches = new Insn[5];
 
-		instructionCount = 0;
-		cycleCount = 0;
+        instructionCount = 0;
+        cycleCount = 0;
     }
 
-	private int additionalMemLatency;
-	private Set<Bypass> bypasses;
-	private Insn[] latches;
-	private int currentMemoryTimer;
-	private long instructionCount;
-	private long cycleCount;
-	private boolean mInsnCanAdvance;
-	private boolean dInsnCanAdvance;
+    private int additionalMemLatency;
+    private Set<Bypass> bypasses;
+    private Insn[] latches;
+    private int currentMemoryTimer;
+    private long instructionCount;
+    private long cycleCount;
+    private boolean mInsnCanAdvance;
+    private boolean dInsnCanAdvance;
 
-	private boolean latchesEmpty(){
-		for(int i = 0; i < 5; i++){
-			if(latches[i] != null) return false;
-		}
-		return true;
-	}
-	private void advanceLatchInsns(Iterator<Insn> instructionIterator){
-		Insn w_Insn = latches[Stage.WRITEBACK.i()];
-		Insn m_Insn = latches[Stage.MEMORY.i()];
-		Insn x_Insn = latches[Stage.EXECUTE.i()];
-		Insn d_Insn = latches[Stage.DECODE.i()];
-		Insn f_Insn = latches[Stage.FETCH.i()];
+    private boolean latchesEmpty(){
+        for(int i = 0; i < 5; i++){
+            if(latches[i] != null) return false;
+        }
+        return true;
+    }
 
-		//WRITEBACK
-		latches[Stage.WRITEBACK.i()] = null;
+    private void clearLatch(Stage s){
+        latches[s.i()] = null;
+    }
 
-		//MEMORY
-		if(mInsnCanAdvance){
-			latches[Stage.WRITEBACK.i()] = latches[Stage.MEMORY.i()];
-			latches[Stage.MEMORY.i()] = null;
-		}
+    private Insn getLatch(Stage s){
+        return latches[s.i()];
+    }
 
-		//EXECUTE
-		if(latches[Stage.MEMORY.i()] == null){
-			latches[Stage.MEMORY.i()] = latches[Stage.EXECUTE.i()];
-			currentMemoryTimer = 0;
-			latches[Stage.EXECUTE.i()] = null;
-		}
+    private void assignLatch(Stage s1, Stage s2){
+        latches[s1.i()] = latches[s2.i()];
+        return;
+    }
 
-		//DECODE
-		if(dInsnCanAdvance && (latches[Stage.EXECUTE.i()] == null)){
-			latches[Stage.EXECUTE.i()] = latches[Stage.DECODE.i()];
-			latches[Stage.DECODE.i()] = null;
-		}
+    private void assignLatch(Stage s1, Insn insn){
+        latches[s1.i()] = insn;
+        return;
+    }
 
-		//FETCH
-		if(latches[Stage.DECODE.i()] == null){
-			latches[Stage.DECODE.i()] = latches[Stage.FETCH.i()];
+    private void advanceLatchInsns(Iterator<Insn> instructionIterator){
+        Insn w_Insn = getLatch(Stage.WRITEBACK);
+        Insn m_Insn = getLatch(Stage.MEMORY);
+        Insn x_Insn = getLatch(Stage.EXECUTE);
+        Insn d_Insn = getLatch(Stage.DECODE);
+        Insn f_Insn = getLatch(Stage.FETCH);
 
-			if(instructionIterator.hasNext()){
-				latches[Stage.FETCH.i()] = instructionIterator.next();
-				instructionCount++;
-			}
-			else {
-				latches[Stage.FETCH.i()] = null;
-			}
-		}
-	}
-	private void checkDelays(){
-		Insn w_Insn = latches[Stage.WRITEBACK.i()];
-		Insn m_Insn = latches[Stage.MEMORY.i()];
-		Insn x_Insn = latches[Stage.EXECUTE.i()];
-		Insn d_Insn = latches[Stage.DECODE.i()];
+        //WRITEBACK
+        clearLatch(Stage.WRITEBACK);
+
+        //MEMORY
+        if(mInsnCanAdvance){
+            assignLatch(Stage.WRITEBACK, Stage.MEMORY);
+            clearLatch(Stage.MEMORY);
+        }
+
+        //EXECUTE
+        if(getLatch(Stage.MEMORY) == null){
+            assignLatch(Stage.MEMORY, Stage.EXECUTE);
+            currentMemoryTimer = 0;
+            clearLatch(Stage.EXECUTE);
+        }
+
+        //DECODE
+        if(dInsnCanAdvance && getLatch(Stage.EXECUTE) == null){
+            assignLatch(Stage.EXECUTE, Stage.DECODE);
+            clearLatch(Stage.DECODE);
+        }
+
+        //FETCH
+        if(getLatch(Stage.DECODE) == null){
+            assignLatch(Stage.DECODE, Stage.FETCH);
+
+            if(instructionIterator.hasNext()){
+                assignLatch(Stage.FETCH, instructionIterator.next());
+                instructionCount++;
+            }
+            else {
+                clearLatch(Stage.FETCH);
+            }
+        }
+    }
+
+    private void checkDelays(){
+        Insn w_Insn = getLatch(Stage.WRITEBACK);
+        Insn m_Insn = getLatch(Stage.MEMORY);
+        Insn x_Insn = getLatch(Stage.EXECUTE);
+        Insn d_Insn = getLatch(Stage.DECODE);
 
 
-		//WRITEBACK
-		//No instruction in the WRITEBACK stage will ever stall.
+        //WRITEBACK
+        //No instruction in the WRITEBACK stage will ever stall.
 
-		//MEMORY
-		//Instructions may stall due to memory latency.
-		if(m_Insn != null && m_Insn.mem != null){
-			mInsnCanAdvance = (currentMemoryTimer >= additionalMemLatency);
-		}
-		else {
-			mInsnCanAdvance = true;
-		}
-		//EXECUTE
-		//EXECUTE will never stall.
-		
-		//DECODE
-		//DECODE may be unable to move to the next stage due to load-use issues.
-		if(bypasses.equals(Bypass.FULL_BYPASS)){
-			if((d_Insn != null) && (x_Insn != null)  && (x_Insn.mem == MemoryOp.Load)
-				&& ((d_Insn.srcReg2 == x_Insn.dstReg) || ((d_Insn.srcReg1 == x_Insn.dstReg) && (d_Insn.mem != MemoryOp.Store)))){
-				dInsnCanAdvance = false;
-			}
-			else {
-				dInsnCanAdvance = true;
-			}
-		}
-		else {
-			//All other combinations of bypasses
-		}
+        //MEMORY
+        //Instructions may stall due to memory latency.
+        if(m_Insn != null && m_Insn.mem != null){
+            mInsnCanAdvance = (currentMemoryTimer >= additionalMemLatency);
+        }
+        else {
+            mInsnCanAdvance = true;
+        }
+        //EXECUTE
+        //EXECUTE will never stall.
 
-		//FETCH
-		//FETCH will never cause a stall.
-	}
+        //DECODE
+        //DECODE may be unable to move to the next stage due to load-use issues.
+        if( bypasses.equals(Bypass.FULL_BYPASS) ){
+            dInsnCanAdvance =
+                ! (d_Insn != null && x_Insn != null && x_Insn.mem == MemoryOp.Load &&
+                   (d_Insn.srcReg2 == x_Insn.dstReg ||
+                    (d_Insn.srcReg1 == x_Insn.dstReg && d_Insn.mem != MemoryOp.Store)));
+        }
+        else {
+            //All other combinations of bypasses
+        }
+
+        //FETCH
+        //FETCH will never cause a stall.
+    }
 
     @Override
     public String[] groupMembers() {
-        return new String[]{"rtian/Richard Zang", "Omar"};
+        return new String[]{"rtian/Richard Zang", "Omar Navarro Leija"};
     }
 
     @Override
     public void run(Iterable<Insn> ii) {
-		Iterator<Insn> instructionIterator = ii.iterator();
-		while(instructionIterator.hasNext() || !latchesEmpty()){
-			checkDelays();
-			advanceLatchInsns(instructionIterator);
+        Iterator<Insn> instructionIterator = ii.iterator();
+        while(instructionIterator.hasNext() || !latchesEmpty()){
+            checkDelays();
+            advanceLatchInsns(instructionIterator);
 
-			currentMemoryTimer++;
-			cycleCount++;
-		}
+            currentMemoryTimer++;
+            cycleCount++;
+        }
     }
 
     @Override
