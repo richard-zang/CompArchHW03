769c74886f6c730b819f853bd154540bc6ae4436
richard-zang <richardzang@gmail.com>
2016-09-29 15:45:27 -0400

    finished all DirPred* and BranchTargetBuffer files

--- src/cis501/submission/BranchTargetBuffer.java
+++ src/cis501/submission/BranchTargetBuffer.java
@@ -4,17 +4,31 @@ import cis501.IBranchTargetBuffer;
 
 public class BranchTargetBuffer implements IBranchTargetBuffer {
 
-    public BranchTargetBuffer(int indexBits) {
+	long[] btbTableTag;
+	long[] btbTableTarget;
 
+	protected int indexBits;
+
+    public BranchTargetBuffer(int indexBits) {
+		this.indexBits = indexBits;
+		btbTableTag = new long[1 << indexBits];
+		btbTableTarget = new long[1 << indexBits];
     }
 
     @Override
     public long predict(long pc) {
-        return 0;
+		//If the tag in the btb table matches the pc, return the target pc.
+        return (btbTableTag[maskedPC(pc)] == pc) ? btbTableTarget[maskedPC(pc)] : 0;
     }
+
+	protected int maskedPC(long pc){
+		int mask = (1 << indexBits) - 1;
+		return (int)pc & mask;
+	}
 
     @Override
     public void train(long pc, long actual) {
-
+		btbTableTarget[maskedPC(pc)] = pc;
+		btbTableTarget[maskedPC(pc)] = actual;
     }
 }
